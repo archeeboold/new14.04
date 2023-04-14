1b7ddc25d46c080e7ae576263790a89ad94ec417
archeeboold <129648148+archeeboold@users.noreply.github.com>
2023-04-14 21:05:24 +0200

    next version

---
+++ src/test/java/Gui.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Gui {
+    public static void main(String[] args) {
+
+        JFrame okno = new JFrame(); //new frame
+        okno.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        okno.setVisible (true);
+        okno.setSize(300, 200);
+        okno.setLocationRelativeTo(null);
+
+        JTextArea textArea = new JTextArea(); //text in this frame
+        textArea.setText("Hey");
+        textArea.setSelectionColor(Color.RED);
+        textArea.setSelectedTextColor(Color.WHITE);
+        textArea.setSelectionStart(0);
+        textArea.setSelectionEnd(3);
+
+        okno.getContentPane().add(textArea); //displaying text in the frame
+    }
+}
+
--- src/test/java/Thor.java
+++ src/test/java/Thor.java
@@ -1,2 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Thor {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Colored Text Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextArea textArea = new JTextArea(10, 30);
+        textArea.setText("This is an example of colored text.");
+        textArea.setSelectionColor(Color.RED);
+        textArea.setSelectedTextColor(Color.WHITE);
+        textArea.setSelectionStart(0);
+        textArea.setSelectionEnd(50);
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
+
